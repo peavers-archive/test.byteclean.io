265b1de579eae48ce53d1560d2865ae04dbbb922
Chris <peavers@gmail.com>
2021-03-06 12:20:58 +0000

    feature: switch to java for diffrent project

--- src/test/java/com/example/byteclean/BytecleanApplicationTests.java
+++ src/test/java/com/example/byteclean/BytecleanApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.byteclean;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BytecleanApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testOne() {
+	}
+
+	@Test
+	void testTwo() {
+	}
+
+	@Disabled
+	void testThree() {
 	}
 
 }
